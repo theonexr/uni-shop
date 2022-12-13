bfcf93ffa48e4b6dc64ec4a8b3000aeb7ce0ea96
theonexr <2684016245@qq.com>
2022-12-13 15:05:30 +0800

    init project

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 官方css库 */
+	@import "common/uni.css";
+	/* 自定义图标库 */
+	@import "common/iconfont.css"
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view >
+		<view>{{333}}</view>
+		<text class="iconfont .icon-shouye" style="font-size: 100rpx; color: red;"></text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
