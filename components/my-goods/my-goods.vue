da7a68c58dec67d3dd8020cf3f2604ceb5926442
theonexr <2684016245@qq.com>
2022-12-16 09:25:25 +0800

    完成商品列表页面开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+	<view>
+		<view class="good_list">
+			
+				<view class="good-item">
+					<!-- 商品左侧图片区域 -->
+					<view class="good-item-left">
+						<image class="goods-pic" :src="goods.goods_small_logo" mode=""></image>
+					</view>
+					<!-- 商品右侧区域 -->
+					<view class="good-item-right">
+						<!-- 商品标题 -->
+						<view class="goods-name">{{goods.goods_name}}</view>
+						<view class="goods-info-box">
+							<!-- 商品价格 -->
+							<view class="goods-price">{{goods.goods_price|tofixed}}</view>
+						</view>
+					</view>
+				</view>
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			goods:{
+				type:Object,
+				default:{}
+			}
+		},
+		filters:{
+			tofixed(num){
+				return Number(num).toFixed(2)
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.good-item{
+	display: flex;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.good-item-left{
+		margin-right: 5px;
+		.goods-pic{
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.good-item-right{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods-name{
+			font-size: 13px;
+		}
+		.goods-info-box{
+			.goods-price{
+				color: #C00000;
+				font-size: 16px
+			}
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		List
+		<view v-for="(goods,i) in goodList" :key="i" @click="gotoDetail(goods)">
+		  <my-goods :goods="goods"></my-goods>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +10,80 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj:{
+					// 查询关键词
+					query:'',
+					// 商品分类id
+					cid:'',
+					// 页码值
+					pagenum:1,
+					// 每页显示多少条数据
+					pagesize:10
+				},
+				// 商品列表的数据
+				goodList:[],
+				// 总数量，实现分页
+				total:0,
+				// 是否正在请求数据
+				isloading:false
 			};
+		},
+		onLoad(options) {
+			// 将页面跳转时的参数转载到queryObj中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			
+			// 获取商品数据
+			this.getGoodsList()
+		},
+		// 触底事件
+		onReachBottom() {
+			// 判断是否有下一页数据
+			if(this.queryObj.pagenum*this.queryObj.pagesize >this.total) return uni.$showMsg('数据加载完毕')
+			// 判断是否在请求其它数据，如果是，则不发起额外请求
+			if(this.isloading) return
+			// 让页码自增
+			this.queryObj.pagenum +=1
+			// 重写获取列表
+			this.getGoodsList()
+
+		},
+		// 下拉刷新事件
+		onPullDownRefresh() {
+			// 1.重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodList = []
+			// 2.重新发起请求
+			this.getGoodsList(()=>
+				uni.stopPullDownRefresh()
+			)
+		},
+		methods:{
+			// 获取数据列表
+			async getGoodsList(cb){
+				// 开启节流阀
+				this.isloading = true
+				const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+				// 请求成功后关闭节流阀
+				this.isloading = false
+				
+				// 数据请求完毕，就立刻按需调用cb回调函数
+				cb && cb()
+				
+				if(res.meta.status!==200) return uni.$showMsg()
+				// 用扩展运算符连接
+				this.goodList = [...this.goodList,...res.message.goods]
+				// 获取总数
+				this.total = res.message.total
+			},
+			gotoDetail(goods){
+				uni.navigateTo({
+					url:'../../subpkg/goods_detail/goods_detail?goods_id='+goods.goods_id
+				})
+			}
 		}
 	}
 </script>
